919e727e60f642c208cfd4d458576e562d2e7c5f
nairb2020 <yhhu@alumni.stanford.edu>
2024-01-11 23:58:31 -0800

    build app version 1

---
+++ src/Components/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.jpeg";
+import { FiArrowRight } from "react-icons/fi";
+import Navbar from "./Navbar";
+import { Button } from "@mui/material";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+
+      <div className="home-banner-container">
+        <div className="home-banner-image-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Splurge Travels Loved by Actual Humans.
+          </h1>
+          <p className="primary-text">
+            Using AI travel concierge to optimize your itinerary to 95% on
+            budget, 5% splurge, and invite-only, real-time collab rewards.
+          </p>
+          <p className="primary-text">
+            Our AI/human solution will intake EVERY single event/piece of
+            property/flight that falls under your itinerary time & locations.
+            From there, using your KYC (anonymized social media feed, LinkedIn,
+            tastes) application, we tailor ideas, based on your needs and travel
+            cadences.
+          </p>
+          <Button
+            variant="contained"
+            size="large"
+            endIcon={<FiArrowRight />}
+            sx={{
+              borderRadius: 4,
+              "&.MuiButton-contained": { background: "#fe9e0e" },
+            }}
+            onClick={() =>
+              window.open(
+                "https://alexander184.typeform.com/to/HUEgef",
+                "_blank"
+              )
+            }
+          >
+            Sign Up Here
+          </Button>
+        </div>
+
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Work.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ChooseImage from "../Assets/choose-image.png";
+
+const Work = () => {
+  const workInfoData = [
+    {
+      image: ChooseImage,
+      title: "Input Your Preference",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+    },
+    {
+      image: ChooseImage,
+      title: "Chat with our AI",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+    },
+    {
+      image: ChooseImage,
+      title: "Splurge and Earn",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+    },
+  ];
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        {/* <p className="primary-subheading">Work</p> */}
+        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={data.image} alt="" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
